b160206fccd3ee08a78f7be33901dde701cdabb1
Христофор <J0muty@mail.ru>
2024-09-24 23:36:40 +0300

    Revert "no underlining, everything is perfect"
    This reverts commit 2f79c0e615d571a3e206f78ed7a0092928b9faad.

--- Coffee/main/urls.py
+++ Coffee/main/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 from .views import CustomLoginView, profile_view
 from django.contrib.auth import views as auth_views
