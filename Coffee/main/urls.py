82757d3a488933d7ee320189ec60677347e88b2b
Христофор <J0muty@mail.ru>
2024-09-26 05:41:31 +0300

    back to no sign up

--- Coffee/main/urls.py
+++ Coffee/main/urls.py
@@ -1,7 +1,5 @@
-from django.urls import path, include
+from django.urls import path
 from . import views
-from .views import CustomLoginView, profile_view
-from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', views.index, name='index'),
@@ -10,8 +8,4 @@ urlpatterns = [
     path('team/', views.team, name='team'),
     path('contact/', views.contact, name='contact'),
     path('application/', views.application, name='application'),
-    path('signup/', views.signup, name='signup'),
-    path('login/', CustomLoginView.as_view(), name='login'),
-    path('profile/', profile_view, name='profile'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
