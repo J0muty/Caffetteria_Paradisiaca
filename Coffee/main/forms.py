78abdc7029be5e3b678b2af51501bd372240a5f4
Христофор <J0muty@mail.ru>
2024-10-02 04:34:17 +0300

    Add new function in forms, add eye in password

--- Coffee/main/forms.py
+++ Coffee/main/forms.py
@@ -1,7 +1,9 @@
 from django import forms
-from django.core.exceptions import ValidationError
 from .models import RegUser
 from datetime import datetime
+from django.contrib.auth.hashers import make_password
+from django.core.exceptions import ValidationError
+import re
 
 
 class RegistrationForm(forms.Form):
@@ -9,19 +11,36 @@ class RegistrationForm(forms.Form):
     last_name = forms.CharField(max_length=30, required=True, widget=forms.TextInput(attrs={'placeholder': 'Фамилия'}))
     email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'placeholder': 'Email'}))
     password = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Пароль'}), required=True)
-    birthdate = forms.CharField(required=True, widget=forms.TextInput(attrs={'placeholder': 'DD-MM-YYYY', 'id': 'birthdate'}))
+    birthdate = forms.CharField(required=True,
+                                widget=forms.TextInput(attrs={'placeholder': 'DD-MM-YYYY', 'id': 'birthdate'}))
+
+    def clean_password(self):
+        password = self.cleaned_data.get('password')
+        if len(password) < 8 or not re.search(r'[A-Z]', password):
+            raise ValidationError('Password must be at least 8 characters long and contain an uppercase letter.')
+        return make_password(password)
 
     def clean_email(self):
         email = self.cleaned_data.get('email')
         if RegUser.objects.filter(email=email).exists():
-            raise ValidationError("Этот адрес электронной почты уже используется.")
+            raise ValidationError('Email is already in use.')
         return email
 
     def clean_birthdate(self):
-        birthday = self.cleaned_data.get('birthdate')
-        if birthday:
+        birthdate = self.cleaned_data.get('birthdate')
+        if birthdate:
             try:
-                return datetime.strptime(birthday, "%d-%m-%Y").date()
+                return datetime.strptime(birthdate, "%d-%m-%Y").date()
             except ValueError:
                 raise ValidationError("Введите правильную дату в формате DD-MM-YYYY.")
         raise ValidationError("Обязательное поле.")
+
+    def save(self):
+        user = RegUser(
+            firstname=self.cleaned_data['firstname'],
+            lastname=self.cleaned_data['lastname'],
+            email=self.cleaned_data['email'],
+            password=make_password(self.cleaned_data['password']),
+            birthdate=self.cleaned_data['birthdate'],
+        )
+        user.save()
