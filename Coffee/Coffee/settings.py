3a9dd6830d824c834e844a4d6bbd6af42d1f97e4
Христофор <J0muty@mail.ru>
2024-10-31 03:00:39 +0300

    from sqllite to mysql :alien:  :+1:

--- Coffee/Coffee/settings.py
+++ Coffee/Coffee/settings.py
@@ -79,8 +79,13 @@ WSGI_APPLICATION = 'Coffee.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'DataBase',
+        'USER': 'root',
+        'PASSWORD': '951753aA.',
+        'HOST': 'localhost',
     }
 }
 
